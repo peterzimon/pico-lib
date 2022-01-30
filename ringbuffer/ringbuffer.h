0c3cca6e0613cedd96dcef9e3b1c1b5678ca0d1d
Peter Zimon <peter.zimon@gmail.com>
2022-01-30 23:51:08 +0100

    Added ringbuffer utility

---
+++ ringbuffer/ringbuffer.h
@@ -0,0 +1,35 @@
+/*
+ * Class: Super-simple™ Ring Buffer
+ * --------------------------------
+ * Stores one byte long data in array (pointer). Specifically made for UART
+ * (MIDI) data buffering. 
+ * ------------------------------------------------------------------------- */
+
+#ifndef _PICO_LIB_RINGBUFFER_H
+#define _PICO_LIB_RINGBUFFER_H
+
+#include <pico/stdlib.h>
+
+class ringbuffer {
+    public: 
+
+        ringbuffer();
+        ~ringbuffer();
+
+        void init(uint8_t* dataBuffer, uint16_t bufferSize);
+
+        bool write_byte(uint8_t data);
+        bool read_byte(uint8_t& data);
+        bool peek(uint8_t& data);
+        bool is_full();
+        bool is_empty();
+
+    private:
+        uint8_t m_buffer_size;
+        uint8_t* m_data_buffer;
+        int m_read_index;
+        int m_write_index;
+        int m_count;
+};
+
+#endif
