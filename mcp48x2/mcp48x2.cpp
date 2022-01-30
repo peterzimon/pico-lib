5ece6044101e376f4a1f88ef8010b3c59e5ba9b2
Peter Zimon <peter.zimon@gmail.com>
2022-01-30 14:55:55 +0100

    Created dummy MCP48X2 library
    - Created first dummy library which can be linked to Pico projects

---
+++ mcp48x2/mcp48x2.cpp
@@ -0,0 +1,4 @@
+#include "mcp48x2.h"
+
+void MCP48X2::init() { }
+void MCP48X2::testFunc() { }
---
+++ mcp48x2/mcp48x2.h
@@ -0,0 +1,12 @@
+#ifndef _MCP48X2_H
+#define _MCP48X2_H
+
+#include <pico/stdlib.h>
+
+class MCP48X2 {
+    public:
+        void init();
+        void testFunc();
+};
+
+#endif
