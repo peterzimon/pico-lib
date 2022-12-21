f026778eac0dc14c939af03590ad628f81482d61
Peter Zimon <peter.zimon@gmail.com>
2022-12-21 21:46:46 +0100

    Added gpio event parser util

--- utils/utils.h
+++ utils/utils.h
@@ -4,6 +4,13 @@
 #include <inttypes.h>
 #include <pico/stdlib.h>
 
+static const char *gpio_irq_str[] = {
+    "LEVEL_LOW",  // 0x1
+    "LEVEL_HIGH", // 0x2
+    "EDGE_FALL",  // 0x4
+    "EDGE_RISE"   // 0x8
+};
+
 class Utils
 {
     public:
@@ -42,6 +49,31 @@ class Utils
                     }
                 }
             }
+        }
+
+        // Convert interrupt event to string
+        // declare this var and use for buf 
+        // static char event_str[128];
+        // Example: https://github.com/raspberrypi/pico-examples/blob/master/gpio/hello_gpio_irq/hello_gpio_irq.c
+        void gpio_event_string(char *buf, uint32_t events) {
+            for (uint i = 0; i < 4; i++) {
+                uint mask = (1 << i);
+                if (events & mask) {
+                    // Copy this event string into the user string
+                    const char *event_str = gpio_irq_str[i];
+                    while (*event_str != '\0') {
+                        *buf++ = *event_str++;
+                    }
+                    events &= ~mask;
+
+                    // If more events add ", "
+                    if (events) {
+                        *buf++ = ',';
+                        *buf++ = ' ';
+                    }
+                }
+            }
+            *buf++ = '\0';
         }
 };
 
