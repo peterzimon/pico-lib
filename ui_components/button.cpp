86c1e08b01e55613c3de38fc239a8f472033dede
Peter Zimon <peter.zimon@gmail.com>
2022-12-25 15:40:28 +0100

    Added Button with debounce to UI components

---
+++ ui_components/button.cpp
@@ -0,0 +1,33 @@
+#include "button.h"
+
+uint32_t Button::get_now_() {
+    return to_ms_since_boot(get_absolute_time());
+}
+
+/* ------------------------ ⬆︎ PRIVATE | PUBLIC ⬇︎  -------------------------- */
+
+Button::Button (uint8_t pin, uint32_t debounce_time) {
+    pin_ = pin;
+    debounce_time_ = debounce_time;
+}
+
+void Button::init_gpio() {
+    gpio_init(pin_);
+    gpio_set_dir(pin_, GPIO_IN);
+    gpio_pull_up(pin_);
+}
+
+bool Button::is_pressed() {
+    bool btn_read = !gpio_get(pin_);
+
+    if (btn_read) {
+        pressed_ = true;
+        previous_debounce_ms_ = get_now_();
+    } else {
+        if (get_now_() - previous_debounce_ms_ > debounce_time_) {
+            pressed_ = false;
+        }
+    }
+
+    return pressed_;
+}
---
+++ ui_components/button.h
@@ -0,0 +1,27 @@
+#ifndef _PICO_LIB_BUTTON_H
+#define _PICO_LIB_BUTTON_H
+
+#include <pico/stdlib.h>
+#include <hardware/gpio.h>
+
+#define DEFAULT_DEBOUNCE_MS 50
+
+class Button
+{
+private:
+    uint8_t pin_;
+    uint32_t previous_debounce_ms_;
+    uint32_t debounce_time_;
+    bool pressed_ = false;
+
+    uint32_t get_now_();
+
+public:
+    Button() { };
+    Button (uint8_t pin, uint32_t debounce_time = DEFAULT_DEBOUNCE_MS);
+
+    void init_gpio();
+    bool is_pressed();
+};
+
+#endif
