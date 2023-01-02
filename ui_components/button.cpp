264e97d14be793b2f7cbd83c10d64b65cec63fb7
Peter Zimon <peter.zimon@gmail.com>
2023-01-02 23:11:32 +0100

    Added double press function to buttons

--- ui_components/button.cpp
+++ ui_components/button.cpp
@@ -19,17 +19,33 @@ void Button::init_gpio() {
 
 bool Button::is_pressed() {
     bool btn_read = !gpio_get(pin_);
+    uint32_t now = get_now_();
 
     if (btn_read) {
         pressed_ = true;
-        previous_debounce_ms_ = get_now_();
+        previous_debounce_ms_ = now;
+        if (pressed_count_ == 0) pressed_count_ = 1;
+        if (pressed_count_ == 1 && now - doublepress_time_ < DOUBLEPRESS_TRESHOLD_MS) {
+            pressed_count_ = 2;
+        }
     } else {
-        if (get_now_() - previous_debounce_ms_ > debounce_time_) {
+        if (now - previous_debounce_ms_ > debounce_time_) {
             pressed_ = false;
+            if (pressed_count_ == 1 && doublepress_time_ == 0) {
+                doublepress_time_ = now;
+            }
+            if (pressed_count_ == 2 || (pressed_count_ == 1 && now - doublepress_time_ > DOUBLEPRESS_TRESHOLD_MS)) {
+                pressed_count_ = 0;
+                doublepress_time_ = 0;
+            }
         }
     }
 
     return pressed_;
+}
+
+bool Button::is_double_pressed() {
+    return pressed_count_ == 2;
 }
 
 bool Button::is_released() {
@@ -47,4 +63,8 @@ bool Button::is_released() {
     }
 
     return released_;
+}
+
+int Button::get_pressed_count() {
+    return pressed_count_;
 }
