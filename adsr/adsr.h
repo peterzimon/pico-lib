fd7e46f3fcddd22a9427faf6f12b1a71485f60a9
Peter Zimon <peter.zimon@gmail.com>
2023-09-08 20:48:21 +0200

    Updated ADSR resolution

--- adsr/adsr.h
+++ adsr/adsr.h
@@ -1,32 +1,32 @@
 /**
  * ADSR class
- * 
+ *
  * Portions of this code are derived from or inspired by the ADSR library
  * developed by mo-thunderz. https://github.com/mo-thunderz/adsr
  * The original code is licensed under the MIT License.
  * Details of the license can be found in the accompanying LICENSE file at https://github.com/mo-thunderz/adsr/blob/main/LICENSE.
- * 
+ *
  * How does triggering work?
- * 
+ *
  * The note_on() acts like a trigger, NOT like a gate. This means that it has to
  * called only once when an ADSR should be started. Similarly, note_off() should
- * be called once when the ADSR should start the release phase. So if you have a 
- * gate like controller then you have to implement this logic. The is_on() 
- * method helps in this. 
- * 
+ * be called once when the ADSR should start the release phase. So if you have a
+ * gate like controller then you have to implement this logic. The is_on()
+ * method helps in this.
+ *
  * A simple implementation can look like this:
- * 
+ *
  * ```
  * ADSR m_adsr(4096);
  * bool gate_on;
- * 
+ *
  * if (gate_on && !m_adsr.is_on()) {
  *      m_adsr.note_on();
  * } else if (gate_off && m_adsr.is_on()) {
  *      m_adsr.note_off();
  * }
  * ```
- * 
+ *
  * The reason for this is to make the class flexible, ie. for being able to re-
  * trigger the ADSR freely.
  * */
@@ -35,10 +35,10 @@
 #define _PICO_LIB_ADSR_H
 
 // Definitions
-#define LUT_SIZE 1024                               // Number of time samples for the lookup tables. It's
+#define LUT_SIZE 4096                               // Number of time samples for the lookup tables. It's
                                                     // essentially the resolution of the ADSR
 #define DEFAULT_ATTACK_ALPHA 0.995                  // Varies between 0.9 (steep curve) and 0.9995 (straight line)
-#define DEFAULT_DECAY_RELEASE 0.972                  // Fits to ARRAY_SIZE 1024
+#define DEFAULT_DECAY_RELEASE 0.99                 // Fits to ARRAY_SIZE 1024
 #define DEFAULT_ADR_uS 300000                       // Default Attack, Decay and Release in us
 #define DEFAULT_SUSTAIN_LEVEL 0.5                   // Relative to max sustain
 
@@ -46,12 +46,12 @@
 
 class ADSR {
 public:
-    
+
     // Constructor
     // Max value of the DAC = 2^resolution. E.g. for a 12bit DAC -> 4096
     ADSR(
-        int dac_size, 
-        float attack_alpha = DEFAULT_ATTACK_ALPHA, 
+        int dac_size,
+        float attack_alpha = DEFAULT_ATTACK_ALPHA,
         float attack_decay_release = DEFAULT_DECAY_RELEASE
     );
 
@@ -66,7 +66,7 @@ public:
     void note_off();
 
     // Options
-    void set_reset_attack(bool l_reset_attack);   // if _reset_attack is true a new trigger starts with 0, 
+    void set_reset_attack(bool l_reset_attack);   // if _reset_attack is true a new trigger starts with 0,
                                                 // if _reset_attack is false it starts with the current output value
 
     bool is_on();
@@ -82,10 +82,10 @@ private:
     float _attack_decay_release;
 
     int _vertical_resolution;                   // number of bits for output, control, etc
-    uint64_t _attack = 0;                  
-    uint64_t _decay = 0;                   
-    int _sustain = 0;                           
-    uint64_t _release = 0;                 
+    uint64_t _attack = 0;
+    uint64_t _decay = 0;
+    int _sustain = 0;
+    uint64_t _release = 0;
     bool _reset_attack = false;
 
     // Time stamp for note on and note off
